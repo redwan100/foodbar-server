d9e1ee605308bdb935f0b92828e295d9d7bd2c79
Redwan islam <redwanislam2760@gmail.com>
2023-05-02 06:54:24 +0600

    crated a server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const data = require('./data/data.json')
+const app = express();
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors())
+
+app.get('/',(reg, res)=>{
+    res.send('chef server is running')
+})
+
+app.get('/category', (reg, res)=>{
+    res.send(data)
+})
+
+app.get('/categories/:id',(reg,res)=>{
+    const id = reg.params.id;
+    const chefItem = data.filter((item)=>item.category_id == id)
+    res.send(chefItem);
+})
+
+app.listen(port, ()=>{
+    console.log('chef server is running');
+})
