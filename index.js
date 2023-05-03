c77c0042250bc622c4d0806fc2985a5564ea891b
Redwan islam <redwanislam2760@gmail.com>
2023-05-03 21:06:39 +0600

    replece json data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const data = require('./data/data.json')
+
+const allData = require('./data/recipe.json');
 const app = express();
 const cors = require('cors');
 const port = 5000;
@@ -11,14 +12,17 @@ app.get('/',(reg, res)=>{
 })
 
 app.get("/category", (reg, res) => {
-  res.send(data);
+  res.send(allData);
 });
+
 
 app.get('/categories/:id',(reg,res)=>{
     const id = reg.params.id;
-    const chefItem = data.filter((item)=>item.id == id)
+    const chefItem = allData.filter((item)=>item.id == id)
     res.send(chefItem);
 })
+
+
 
 app.listen(port, ()=>{
     console.log('chef server is running');
